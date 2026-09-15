8eeb4c1671c823249abc4b28af704533c0a87cb9
Raquel Calazans <raqcalazans@gmail.com>
2026-09-15 20:53:19 -0300

    delete api

--- api/brasileirao-api/src/main/java/com/selecaoglobocom/brasileirao_api/repository/GameEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.selecaoglobocom.brasileirao_api.repository;
-
-import com.selecaoglobocom.brasileirao_api.entity.GameEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameEventRepository extends JpaRepository<GameEvent, Integer> {
-}
--- api/brasileirao-api/src/main/java/com/selecaoglobocom/brasileirao_api/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.selecaoglobocom.brasileirao_api.repository;
-
-import com.selecaoglobocom.brasileirao_api.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-}
